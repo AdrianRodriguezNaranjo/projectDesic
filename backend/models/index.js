815d191e2fdef45b4b2000cd72e2e945f51c269b
Naidr <adrianrodrigueznaranjo@alumno.ieselrincon.es>
2023-11-15 16:10:25 +0000

    Inicial backend

---
+++ backend/models/index.js
@@ -0,0 +1,22 @@
+const dbConfig = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB,dbConfig.USER, dbConfig.PASSWORD, {
+    host:dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+    
+    pool: {
+        max: dbConfig.max,
+        min : dbConfig.min,
+        acquire: dbConfig.acquire,
+        idle: dbConfig.idle
+    }
+})
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
